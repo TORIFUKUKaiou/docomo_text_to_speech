7d46056298cdd793b6fa3e6cf76462bfb5f5a3f0
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2020-04-19 09:50:48 +0900

    version up

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DocomoTextToSpeech.MixProject do
   def project do
     [
       app: :docomo_text_to_speech,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
