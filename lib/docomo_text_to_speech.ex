2691aa23052bac3596b7a2d0cc2d8a7f39784551
Kian-Meng, Ang <kianmeng@cpan.org>
2020-10-10 20:08:20 +0800

    Refactor and replace the HTTP client
    List of changes:
    
    - Switch HTTP client from HTTPoison to Tesla
    - Use concise way to handle URL params
    - Fix and update ExVCR config, test cases, and cached result
    - Remove all unused config by environment
    - Follow the Tesla formatter rules
    - Filter sensitive API token from cached HTTP response

--- lib/docomo_text_to_speech.ex
+++ lib/docomo_text_to_speech.ex
@@ -4,10 +4,39 @@ defmodule DocomoTextToSpeech do
              |> String.split("<!-- MDOC !-->")
              |> Enum.fetch!(1)
 
+  use Tesla, only: [:post], docs: false
+
+  adapter Tesla.Adapter.Hackney
+  plug Tesla.Middleware.BaseUrl, "https://api.apigw.smt.docomo.ne.jp/futureVoiceCrayon/v1"
+  plug Tesla.Middleware.Query, APIKEY: api_key()
+  plug Tesla.Middleware.JSON
+
+  @default_opts [
+    command: "AP_Synth",
+    speaker_id: 1,
+    style_id: 1,
+    speech_rate: 1.0,
+    power_rate: 1.0,
+    voice_type: 1.0,
+    audio_file_format: 2
+  ]
+
+  @typedoc "Text to speech conversion option"
+  @type conversion_option ::
+          {:command, String.t()}
+          | {:speaker_id, pos_integer()}
+          | {:style_id, pos_integer()}
+          | {:speech_rate, float()}
+          | {:power_rate, float()}
+          | {:voice_type, float()}
+          | {:audio_file_format, pos_integer()}
+
   @doc """
-  Converts text to speech
+  Converts text to speech.
 
   ## Examples
+
+  With valid API key.
 
       iex> DocomoTextToSpeech.run "エリクサー"
       {:ok,
@@ -15,54 +44,78 @@ defmodule DocomoTextToSpeech do
        0, 1, 0, 1, 0, 34, 86, 0, 0, 68, 172, 0, 0, 2, 0, 16, 0, 0, 0, 100, 97, 116,
        97, 246, 198, 0, 0, 0, 0, ...>>}
 
+  Without valid API key.
+
+      iex> DocomoTextToSpeech.run "エリクサー"
+      {:error, "(401) POLSLA009: Unable to perform ApiKey based Authentication"}
+
+  Without `APIKEY` URL param.
+
+      iex> DocomoTextToSpeech.run "エリクサー"
+      {:error, "(401) POLSLA009: APIKEY is null or empty in the request"}
+
+      iex(5)> DocomoTextToSpeech.run! "エリクサー"
+      ** (RuntimeError) (401) POLSLA009: Unable to perform ApiKey based Authentication
+          (docomo_text_to_speech) lib/docomo_text_to_speech.ex:97: DocomoTextToSpeech.run!/2
+
   """
-  def run(
-        text,
-        speaker_id \\ 1,
-        style_id \\ 1,
-        speech_rate \\ 1.0,
-        power_rate \\ 1.0,
-        voice_type \\ 1.0,
-        audio_file_format \\ 2
-      ) do
-    DocomoTextToSpeech.Api.post(
-      text,
-      speaker_id,
-      style_id,
-      speech_rate,
-      power_rate,
-      voice_type,
-      audio_file_format
-    )
-    |> _format_current()
+  @spec run(String.t(), [conversion_option()]) :: {:ok | :error, any()}
+  def run(text, opts \\ []) do
+    params =
+      @default_opts
+      |> Keyword.merge(opts)
+      |> Keyword.put_new(:text_data, text)
+      |> Enum.into(%{}, fn {k, v} ->
+        {k |> to_string() |> camelize(), v}
+      end)
+
+    "/textToSpeech"
+    |> post(params)
+    |> handle_response()
   end
 
   @doc """
-  Converts text to speech
+  Returns the audio file directly or raise exception if any errors.
 
   ## Examples
 
-      iex> DocomoTextToSpeech.run "エリクサー"
+  Successfull convertion.
+
+      iex> DocomoTextToSpeech.run! "エリクサー"
       <<82, 73, 70, 70, 28, 199, 0, 0, 87, 65, 86, 69, 102, 109, 116, 32, 18, 0, 0,
         0, 1, 0, 1, 0, 34, 86, 0, 0, 68, 172, 0, 0, 2, 0, 16, 0, 0, 0, 100, 97, 116,
         97, 246, 198, 0, 0, 0, 0, ...>>
 
-  """
-  def run!(
-        text,
-        speaker_id \\ 1,
-        style_id \\ 1,
-        speech_rate \\ 1.0,
-        power_rate \\ 1.0,
-        voice_type \\ 1.0,
-        audio_file_format \\ 2
-      ) do
-    {:ok, content} =
-      run(text, speaker_id, style_id, speech_rate, power_rate, voice_type, audio_file_format)
+  Failed conversion.
 
-    content
+      iex(4)> DocomoTextToSpeech.run! "エリクサー"
+      ** (RuntimeError) (401) POLSLA009: APIKEY is null or empty in the request
+          (docomo_text_to_speech) lib/docomo_text_to_speech.ex:96: DocomoTextToSpeech.run!/2
+
+
+  """
+  @spec run!(String.t(), [conversion_option()]) :: {:ok | :error, any()} | no_return()
+  def run!(text, opts \\ []) do
+    case run(text, opts) do
+      {:ok, content} -> content
+      {:error, msg} -> raise msg
+    end
   end
 
-  defp _format_current({:ok, body, _}), do: {:ok, body}
-  defp _format_current(error), do: error
+  defp api_key do
+    System.get_env("DOCOMO_TEXT_TO_SPEECH_API_KEY") ||
+      Application.get_env(:docomo_text_to_speech, :api_key)
+  end
+
+  defp camelize("speaker_id"), do: "SpeakerID"
+  defp camelize("style_id"), do: "StyleID"
+  defp camelize(key), do: Macro.camelize(key)
+
+  defp handle_response({:error, _} = error), do: error
+  defp handle_response({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, body}
+
+  defp handle_response({:ok, %Tesla.Env{status: status, body: body}}) do
+    %{"requestError" => %{"policyException" => %{"messageId" => msg_id, "text" => msg}}} = body
+    {:error, "(#{status}) #{msg_id}: #{msg}"}
+  end
 end
--- test/docomo_text_to_speech_test.exs
+++ test/docomo_text_to_speech_test.exs
@@ -1,28 +1,39 @@
 defmodule DocomoTextToSpeechTest do
-  use ExUnit.Case
   use ExUnit.Case, async: true
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
-  doctest DocomoTextToSpeech, except: [run: 7, run!: 7]
-  doctest DocomoTextToSpeech.Parser
 
   @text "I Was Born To Love Elixir"
 
-  setup_all do
-    HTTPoison.start()
+  describe "run/2" do
+    setup [:invalid_api_key, :remove_api_key]
+
+    test "invalid api key" do
+      use_cassette "invalid_api_key" do
+        expect = {:error, "(401) POLSLA009: Unable to perform ApiKey based Authentication"}
+        assert DocomoTextToSpeech.run(@text) == expect
+      end
+    end
   end
 
-  test "run" do
-    use_cassette "httpoison_post" do
-      {:ok, body} = DocomoTextToSpeech.run(@text)
-      assert body
-    end
+  describe "run!/2" do
+    setup [:invalid_api_key, :remove_api_key]
 
-    use_cassette "httpoison_post_error" do
-      {:error, body, %{"Content-Type" => "application/json"}, status_code} =
-        DocomoTextToSpeech.run(@text)
-
-      assert body
-      assert status_code
+    test "raise exception invalid api key" do
+      use_cassette "invalid_api_key" do
+        assert_raise RuntimeError, fn ->
+          DocomoTextToSpeech.run!(@text)
+        end
+      end
     end
+  end
+
+  defp remove_api_key(_context) do
+    ExVCR.Config.filter_sensitive_data("(?<=APIKEY=)([^&]*)", "<REDACTED>")
+    :ok
+  end
+
+  defp invalid_api_key(_context) do
+    System.put_env("DOCOMO_TEXT_TO_SPEECH_API_KEY", "<secret>")
+    :ok
   end
 end
